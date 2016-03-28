16c25662c674011272f9e62a46d0aa8145ab6e27
luozc-kf1b <zhcluo@gmail.com>
2016-03-28 15:41:19 +0800

    05.js

--- 3145/05/05.js
+++ 3145/05/05.js
@@ -18,4 +18,40 @@ $(document).ready(function(){
     );
     $('<a href="#top">back to top</a>').insertAfter('div.chapter p');
     $('<a id="top"></a>').prependTo('body');
+    /*$('span.footnote').insertBefore('#footer').wrapAll('<ol id="notes"></ol>').wrap('<li></li>');*/
+    var $notes = $('<ol id="notes"></ol>').insertBefore('#footer');
+    $('span.footnote').each(function(index){
+        $(this).before([
+            '<a href="#footernote-',
+            index+1,
+            '" id="context-',
+            index+1,
+            '" class="context">',
+            '<sup>',
+            index+1,
+            '</sup></a>'
+        ].join(''))
+            .appendTo($notes)
+            .append(
+                [
+                    '&nbsp;(<a href="#ontext-',
+                    index+1,
+                    '">context</a>)'
+                ].join('')
+            )
+            .wrap('<li id="footnote-'+(index+1)+'"></li>');
+
+    });
+    $('span.pull-quote').each(function(index){
+        var $parentParagraph = $(this).parent('p');
+        $parentParagraph.css('position','relative');
+        var $clonedCopy = $(this).clone();
+        $clonedCopy.addClass('pulled')
+            .find('span.drop')
+            .html('&hellip;')
+            .end()
+            .text($clonedCopy.text())
+            .prependTo($parentParagraph);
+        
+    });
 });
