9663d923a2d1c07b3ec3a67eaf488c4ea2f28e28
luozc-kf1b <zhcluo@gmail.com>
2016-04-06 16:44:11 +0800

    07.js

--- 3145/07/07.js
+++ 3145/07/07.js
@@ -4,3 +4,29 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$.fn.cycle.defaults.timeout = 10000;
+$.fn.cycle.defaults.random = true;
+$(document).ready(function(){
+
+    $('#books').cycle({
+        timeout:1000,
+        speed:200,
+        pause:true
+    });
+
+    var $books = $('#books');
+    var $controls = $('<div id="books-controls"></div>');
+    $controls.insertAfter($books);
+    $('<button>Pause</button>').click(function(event){
+        event.preventDefault();
+        $books.cycle('pause');
+        $.cookie('cyclePaused','y');
+    }).appendTo($controls);
+    $('<button>Resume</button>').click(function(event){
+        event.preventDefault();
+        // $books.cycle('resume');
+        $('ul:paused').cycle('resume');
+        $.cookie('cyclePaused',null);
+    }).appendTo($controls);
+
+});
