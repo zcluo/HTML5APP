a616f1b0ee969a27ade504809f245890d7433396
zhcluo <zhcluo@gmail.com>
2016-05-22 21:52:21 +0800

    tree

--- bootstrap/js/3.js
+++ bootstrap/js/3.js
@@ -2,17 +2,52 @@
  * Created by luozc-kf1b on 2016/4/28.
  */
 $(document).ready(function () {
-    var url1 = 'http://localhost:8080/ajaxserverjsonp/user/re';
-    $.ajax(url1,{
-        dataType:'jsonp',
-        data:{"id":1},
-        //jsonpCallback: 'JsonpCallback',
-        contentType: 'application/jsonp;charset=UTF-8',
-        success:function (data) {
-            //console.log(JSON.parse(data));
-            //var ret = JSON.parse(data);
-            console.log(data["127"].name);
+    var data = [];
+    function buildDomTree() {
 
+
+
+
+        function walk(nodes, data) {
+            if (!nodes) { return; }
+            $.each(nodes, function (id, node) {
+                var obj = {
+                    id: id,
+                    text: node.nodeName + " - " + (node.textContent ? node.textContent : ''),
+                    tags: [node.childElementCount > 0 ? node.childElementCount + ' child elements' : '']
+                };
+                if (node.childElementCount > 0) {
+                    obj.nodes = [];
+                    walk(node.childNodes, obj.nodes);
+                }
+                data.push(obj);
+            });
         }
+        //walk($('html')[0].children, data);
+        $.ajax({
+            type: "GET",
+            url: "data/categories.xml",
+            cache: false,
+            async:false,
+            dataType: "xml",
+            success: function(xml) {
+                $(xml).find("Categories").each(function(){
+                    var name = $(this).find("CategoryName").text();
+                    console.log(name);});
+                console.log($(xml).find("Categories").find("CategoryName").text());
+                walk($(xml)[0].childNodes,data);
+            }
+        });
+        return data;
+    }
+    $(function() {
+        var options = {
+            bootstrap2: false,
+            showTags: true,
+            levels: 5,
+            data: buildDomTree()
+        };
+        $('#treeview').treeview(options);
     });
+
 });
